e9bbe54a1f0627c9eaf656888f4546cadcabb414
AugustusHsu <jimhsu11@gmail.com>
2017-04-03 22:35:25 +0800

    Merge測試

--- src/graphical.py
+++ src/graphical.py
@@ -6,10 +6,6 @@ Created on Mon Apr  3 20:45:44 2017
 @author: jim
 """
 import pandas as pd 
-df = pd.DataFrame
-df.columns = ['日期','成交股數','成交金額','開盤價','最高價','最低價','收盤價','漲跌價差','成交筆數']
 df = pd.read_csv("../data/2330.csv")
 
 print(df)
-
-print(df.columns)
