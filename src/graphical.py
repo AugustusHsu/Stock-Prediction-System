9e58f11105baf1c34ff307235f9a311b988570b6
AugustusHsu <jimhsu11@gmail.com>
2017-04-04 21:51:23 +0800

    新增graphical.py圖表化資料 呼叫Graphical_Stock_DATA圖表化資料

--- src/graphical.py
+++ src/graphical.py
@@ -4,14 +4,10 @@
 import os
 import pandas as pd 
 import matplotlib.pyplot as plt
-
-def Graphical_Stock_DATA(Stock_ID):
+#圖表化的png名稱、要圖表化的資料
+def Graphical_Stock_DATA(PNGname,y):
     if not os.path.isdir('../grp'):
         os.makedirs('../grp')
-    filepath = "../data/" + Stock_ID + ".csv"
-    print(filepath)
-    df = pd.read_csv(filepath)
-    y = df['收盤價']
     x = range(len(y))
     # 開始畫圖
         # 設定要畫的的x,y數據list....
@@ -22,13 +18,20 @@ def Graphical_Stock_DATA(Stock_ID):
         # 照需要寫入x 軸和y軸的 label 以及title
     plt.xlabel("Day") 
     plt.ylabel("Stock price") 
-    plt.title(Stock_ID) 
+    plt.title(PNGname) 
         # 在這個指令之前，都還在做畫圖的動作 
         # 這個指令算是 "秀圖" 
     #plt.show() 
         # 如果要存成圖形檔:
         # 把 pyplot.show() 換成下面這行:
-    savefigpath = "../grp/" + Stock_ID + ".png"
-    plt.savefig(savefigpath,dpi=300,format="png") 
+    savefigpath = "../grp/" + PNGname + ".png"
+    plt.savefig(savefigpath,dpi=500,format="png") 
+    plt.clf()
 
-Graphical_Stock_DATA("2330")
+Stock_ID = ["2330","2002"]
+for ID in Stock_ID:
+    filepath = "../data/" + ID + ".csv"
+    print(filepath)
+    df = pd.read_csv(filepath)
+    y = df['收盤價']
+    Graphical_Stock_DATA(ID,y)
